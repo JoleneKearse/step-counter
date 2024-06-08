ae5bed76d8de27556e8ca5c1e88afcdc32bf9386
JoleneKearse <pharaohnutz@gmail.com>
2024-06-08 17:22:38 -0400

    chore: update input sizes

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,14 @@
 export function Counter() {
   return (
-    <form className="flex flex-col items-center w-3/5 gap-10 p-10 rounded-3xl bg-neutral-800">
-      <label htmlFor="daily-steps" className="text-neutral-200">
+    <form className="flex flex-col items-center w-3/5 gap-10 p-10 rounded-3xl bg-neutral-800 lg:w-1/4">
+      <label htmlFor="daily-steps" className="leading-5 text-neutral-200">
         How many steps did you take today?
       </label>
       <input
         type="number"
         name="daily-steps"
         id="daily-steps"
-        className="px-4 py-1 font-bold rounded-md text-neutral-800 bg-neutral-200 "
+        className="w-3/5 px-4 py-1 font-bold text-center rounded-md text-neutral-800 bg-neutral-200 md:w-2/5 lg:w-1/3"
       />
       <button
         className="inline-block px-3 py-1 rounded-md w-fit bg-gradient-to-br from-orange via-bright-salmon to-dusty-pink hover:bg-gradient-to-tl"
