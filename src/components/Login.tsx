b2a8fdcd3df7c42782a66b03c7d61de794091036
JoleneKearse <pharaohnutz@gmail.com>
2024-06-09 15:23:19 -0400

    chore: add login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,25 @@
 export function Login() {
   return (
-    <section>
-      <img 
-        src="/logo.svg" 
-        alt="steps on counters" 
-      />
+    <section className="flex flex-col items-center gap-2 px-4 -mt-5">
+      <p className="text-neutral-200">
+        You signed up for a stepping challenge - hooray!
+      </p>
+      <p className="text-neutral-200">
+        You do the hard work, and we'll cheer you on while doing the tracking.
+      </p>
+      <p className="text-neutral-200">
+        Sign up, pick a timeframe & overall goal.
+      </p>
+      <p className="text-neutral-200">
+        We'll break it into a daily goal and track when you're below, above, or
+        bang on!
+      </p>
+      <img src="/logo.svg" alt="sneakers walking" className="-mt-5" />
+      <button
+        className="inline-block px-3 py-1 border border-4 rounded-md border-neutral-900 w-fit bg-gradient-to-br from-orange via-bright-salmon to-dusty-pink hover:bg-gradient-to-tl"
+      >
+        Login with Google!
+      </button>
     </section>
-  )
+  );
 }
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,14 +1,9 @@
 export function Title() {
   return (
     <header className="py-10 text-center">
-      <h1 className="inline-block mb-10 text-3xl font-extrabold text-transparent bg-gradient-to-br from-orange via-bright-salmon to-dusty-pink bg-clip-text">
+      <h1 className="inline-block text-3xl font-extrabold text-transparent bg-gradient-to-br from-orange via-bright-salmon to-dusty-pink bg-clip-text">
         Step Counter
       </h1>
-      <p className="text-neutral-200">Just pick a timeframe & overall goal.</p>
-      <p className="text-neutral-200">
-        We'll break it into a daily goal and track when you're below, above, or
-        bang on!
-      </p>
     </header>
   );
 }
